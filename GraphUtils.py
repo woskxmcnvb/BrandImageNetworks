bd7cbf81868f1c9ad25f357998600a8e5227b252
Vladimir Bondarenko <VBondarenko@armi-marketing.com>
2024-04-18 14:00:24 +0300

    Graphic model initially built

--- GraphUtils.py
+++ GraphUtils.py
@@ -15,98 +15,165 @@ from plspm.mode import Mode
 
 import utils
 
+def PlotGraph(graph: pgv.AGraph):
+    stream = io.BytesIO()
+    graph.draw(stream, format='png', prog='dot')
+    return Image.open(stream)
+
 
 class ModelSpec:
-    PATH_EDGE_TYPE = 'path'
+
+    class Edge:
+        TYPES = ['path', 'correlation']
+        def __init__(self, from_: str, to_: str, type: str):
+            assert type in self.TYPES, "Wrong path type definition: {} to {}".format(from_, to_)
+            self.from_ = from_
+            self.to_ = to_
+            self.type_ = type
+        
+        def AsTouple(self, include_type=False): 
+            if include_type:
+                return (self.from_, self.to_, self.type_)
+            else:
+                return (self.from_, self.to_)
 
     class Construct:
-        TYPES = ['formative', 'reflective']
+        TYPE_FORMATIVE = 'formative'
+        TYPE_REFLECTIVE = 'reflective'
+        TYPES = [TYPE_FORMATIVE, TYPE_REFLECTIVE]
         def __init__(self, name: str, type: str, indicators: list | str):
-            self.type = type
-            self.name = name
-            if isinstance(indicators, list):
-                self.indicators = indicators.copy()
+            self.type_ = type
+            self.name_ = name
+            self.indicators_ = set()
+            if isinstance(indicators, list) or isinstance(indicators, set):
+                self.indicators_.update(indicators)
             elif isinstance(indicators, str):
-                self.indicators = [indicators]
+                self.indicators_.add(indicators)
             else:
-                raise ValueError("Wrong LV definition")
+                raise ValueError("Wrong LV definition {}".format(name))
+            
+        def Indicators(self):
+            return self.indicators_
 
         def __repr__(self):
-            return "{} {} {}".format(self.name, self.type, self.indicators)
+            return "{} {} {}".format(self.name_, self.type_, self.indicators_)
 
         def AppendIndicator(self, name: str, type: str, indicator: str):
-            assert (self.type == type) and (self.name == name), "Inconsistent LV definition"
-            self.indicators.append(indicator)
+            assert isinstance(indicator, str)
+            assert (self.type_ == type) and (self.name_ == name), "Inconsistent LV definition {} {} {}".format(name, type, indicator)
+            self.indicators_.add(indicator)
 
+    class Node: 
+        name: str
+        in_edges: set[str]
+        out_edges: set[str]
+        def __init__(self, name: str):
+            self.name = name
+            self.in_edges = set()
+            self.out_edges = set()
+        
+        def AddInEdge(self, from_node: str):
+            self.in_edges.add(from_node)
+        
+        def AddOutEdge(self, to_node: str):
+            self.out_edges.add(to_node)
+        
+        def InEdges(self): 
+            return self.in_edges
+        
+        def OutEdges(self):
+            return self.out_edges
 
-    edges: list[tuple[str, str]]
+    edges: list[Edge]
     constructs: dict[str, Construct]
+    nodes: dict[str, Node]
 
     def __init__(self, spec: list[dict]) -> None:
         # accepts list of model edges
         # dict format: {'from': str, 'to': str, 'type': str}
         self.edges = list()
         self.constructs = dict()
-        self.__AppendFromList(spec)
+        self.nodes = dict()
+        if isinstance(spec, pd.DataFrame):
+            self.AppendFromDF(spec)
+        elif isinstance(spec, list):
+            self.AppendFromList(spec)
+        self.ConsistencyCheck()
 
-    def __AppendFromList(self, spec: list[dict]):
+    def Edges(self, include_type=False) -> list[tuple]:
+        return [e.AsTouple() for e in self.edges]
+    
+    def Nodes(self) -> list[str]:
+        return list(self.nodes.keys()) 
+    
+    def HasConstruct(self, name: str) -> bool:
+        return name in self.constructs.keys()
+    
+    def InEdges(self, node) -> set[str]:
+        return self.nodes[node].InEdges()
+    
+    def OutEdges(self, node) -> set[str]:
+        return self.nodes[node].OutEdges()
+    
+    def AppendFromDF(self, spec: pd.DataFrame): 
+        self.AppendFromList(spec.to_dict('records'))
+
+    def __AddNode(self, name: str):
+        if not name in self.nodes.keys():
+            self.nodes[name] = self.Node(name)
+
+    def AppendFromList(self, spec: list[dict]):
+        FROM, TO, TYPE  = 'from', 'to', 'type'
+        REQUIRED_KEYS = [FROM, TO, TYPE]
+
         for item in spec: 
-            if item['type'] in  self.Construct.TYPES:
-                if item['from'] in self.constructs.keys():
-                    self.constructs[item['from']].AppendIndicator(item['from'], item['type'], item['to'])
+            from_, to_, type_ = item[FROM], item[TO], item[TYPE]
+            if type_ in self.Construct.TYPES:
+                if self.HasConstruct(from_):
+                    self.constructs[from_].AppendIndicator(from_, type_, to_)
                 else:
-                    self.constructs[item['from']] = self.Construct(item['from'], item['type'], item['to'])
-            elif item['type'] == self.PATH_EDGE_TYPE:
-                self.edges.append((item['from'], item['to']))
+                    self.constructs[from_] = self.Construct(from_, type_, to_)
+            elif type_ in self.Edge.TYPES:
+                self.edges.append(self.Edge(from_, to_, type_))
+                self.__AddNode(from_)
+                self.__AddNode(to_)
+                self.nodes[to_].AddInEdge(from_)
+                self.nodes[from_].AddOutEdge(to_)
             else:
                 raise ValueError("Wrong LV definition: {}".format(item))
+            
+    def ConsistencyCheck(self):
+        all_mvs_for_constructs = set()
+        for c in self.constructs.values():
+            doubles = all_mvs_for_constructs.intersection(c.indicators_)
+            if doubles:
+                raise ValueError("Inconsistent model spec: Double use of indicators in constructs {}".format(doubles))
+            all_mvs_for_constructs.update(c.indicators_)
+        
+        all_mvs_in_single_lvs = set()
+        for e in self.Edges(include_type=False):
+            all_mvs_in_single_lvs.update(e)
+        doubles = all_mvs_for_constructs.intersection(all_mvs_in_single_lvs)
+        if doubles:
+            raise ValueError("Inconsistent model spec: The same indicators in single and constructs {}".format(doubles))
+
 
 
 class PLSPMModel:
-    model: Plspm | None
-    config: c.Config | None
+    model: Plspm | None = None
+    config: c.Config | None = None
 
-    def Reset(self):
-        self.structure = c.Structure()
+    def __init__(self, scaled=False):
         self.nodes = list()
-        self.model = None
-        self.config = None
-
-    def __init__(self, edges=None, scaled=False):
-        self.Reset()
         self.scaled = scaled
-        if edges:
-            self.ConfigFromEdges(edges)
-
-    def ConfigFromEdges(self, edges: list): 
-        ##########
-        ########## переделать
-        ##########
-        assert self.config is None, "Already configured"
-
-        # path model
-        for from_, to_ in edges: 
-            self.structure.add_path(['lv_{}'.format(from_)], ['lv_{}'.format(to_)])
-
-        self.config = c.Config(self.structure.path(), scaled=self.scaled)
-
-        # measurement model 
-        for nodes in edges: 
-            for node in nodes:
-                if node not in self.nodes:
-                    self.config.add_lv('lv_{}'.format(node), Mode.A, c.MV(node))
-                    self.nodes.append(node)
-
-        return self
 
     def ConfigFromModel(self, model: ModelSpec): 
-        self.Reset() 
         
         lvs_to_create = set()
-        mvs_already_created = set()
 
         # path model
-        for from_, to_ in model.edges: 
+        self.structure = c.Structure()
+        for from_, to_ in model.Edges(include_type=False): 
             self.structure.add_path(['lv_{}'.format(from_)], ['lv_{}'.format(to_)])
             lvs_to_create.add(from_)  
             lvs_to_create.add(to_)
@@ -115,24 +182,13 @@ class PLSPMModel:
         
         # composite latent variables 
         for name, lv in model.constructs.items():
-            # inconsistency check
-            for i in lv.indicators:
-                if i in mvs_already_created:
-                    raise ValueError('Problem with {}: double use'.format(i))
-            mvs_already_created.update(lv.indicators)
-                
-            mvs = [c.MV(i) for i in lv.indicators]
-            mode = Mode.A if lv.type == 'formative' else 'reflective'
+            mvs = [c.MV(i) for i in lv.Indicators()]
+            mode = Mode.A if lv.type_ == ModelSpec.Construct.TYPE_FORMATIVE else Mode.B
             self.config.add_lv('lv_{}'.format(name), mode, *mvs)
             lvs_to_create.remove(name)
 
         # the rest - single indicator variables
         for lv in lvs_to_create:
-            # inconsistency check 
-            if lv in mvs_already_created:
-                raise ValueError('Problem with {}: double use'.format(lv))
-            mvs_already_created.add(lv)
-
             self.config.add_lv('lv_{}'.format(lv), Mode.A, c.MV(lv))
 
         return self
@@ -146,132 +202,71 @@ class PLSPMModel:
         assert self.model, "Run .Fit first"
         return self.model.path_coefficients()
     
-    def GetEdgeCoef(self, edge: tuple):
+    def GetPathCoef(self, edge: tuple):
         assert self.model, "Run .Fit first"
         from_, to_ = ['lv_{}'.format(node) for node in edge]
         return self.GetPathCoefs().loc[to_, from_]
 
 
-class GraphManipulations:
+class GraphicModel:
     graph: pgv.AGraph | None = None
     plspm: PLSPMModel | None = None
     model_spec: ModelSpec | None = None
 
-    def __FromEdges(self, edges: list):
-        self.graph = pgv.AGraph(directed=True)
-        self.graph.add_edges_from(edges)
-
-
-    @classmethod
-    def FromSpec(self, spec: pd.DataFrame):
-        FROM, TO, TYPE  = 'from', 'to', 'type'
-        REFL, FORM, PATH = 'reflective', 'formative', 'path'
-
-        assert utils.CheckColumnsPresent(spec, [FROM, TO, TYPE])
-        
-        constructs = dict()
-        
-        graph = pgv.AGraph(directed=True)
-
-        for _, edge in spec.iterrows(): 
-            if edge[TYPE] == PATH:
-                graph.add_edge(edge[FROM], edge[TO])
-            elif edge[TYPE] in [REFL, FORM]:
-                if edge[FROM] in constructs.keys(): 
-                    assert constructs[edge[FROM]]['type'] == edge[TYPE], "Inconsistent LV definition {}".format(edge[FROM])
-                    constructs[edge[FROM]]['mvs'].append(edge[TO])
-                else: 
-                    constructs[edge[FROM]] = {'type': edge[TYPE], 'mvs': [edge[TO]]}
-
-        for c_name, c_def in constructs.items():
-            #graph.add_nodes_from(c_def['mvs'])
-            sg = graph.add_subgraph(name=c_name, label=c_def[TYPE], bgcolor='red')
-            sg.add_nodes_from(c_def['mvs'])
-        
-        return graph
-
-
-    
-    def __init__(self, graph): 
-        if isinstance(graph, pgv.AGraph): 
-            self.graph = graph.copy()
-        elif isinstance(graph, list): 
-            self.__FromEdges(graph)
-        elif isinstance(graph, ModelSpec):
-            self.model_spec = graph
-            self.__FromEdges(graph.edges)
-        else: 
-            self.graph = graph.to_graphviz()
-
-
-
+    def __init__(self, spec): 
+        self.model_spec = ModelSpec(spec)
 
     def ResetModels(self):
         self.plspm = None
-
-    def AddEdges(self, edges: list): 
-        self.ResetModels()
-        self.graph.add_edges_from(edges)
-
-    def AddEdge(self, edge: tuple):
-        self.ResetModels()
-        self.graph.add_edge(*edge)
-    
-    def AddEdgeWeight(self, edge, weight): 
-        assert self.graph.has_edge(*edge), "No edge " + str(edge)
-        e = self.graph.get_edge(*edge)
-        e.attr['label'] = '{:.2f}'.format(weight)
-        e.attr['penwidth'] = weight * 10
-    
-    def Edges(self):
-        return self.graph.edges()
-    
-    def Plot(self):
-        stream = io.BytesIO()
-        self.graph.draw(stream, format='png', prog='dot')
-        return Image.open(stream)
-    
+  
     def FitPLSPM(self, data: pd.DataFrame): 
-        # check all nodes in columns
-        # некоторая черезжопность
-        if self.model_spec:
-            self.plspm = PLSPMModel().ConfigFromModel(self.model_spec).Fit(data)
+        self.plspm = PLSPMModel().ConfigFromModel(self.model_spec).Fit(data)
+        return self
+
+    def Graph(self, add_pls_weights=True):
+        if add_pls_weights and (self.plspm is None):
+            print("Warning! PLS is not fit, building without PLS weights")
+        
+        graph = pgv.AGraph(directed=True)
+        if add_pls_weights: 
+            for edge in self.model_spec.Edges(include_type=False):
+                wt = self.plspm.GetPathCoef(edge)
+                graph.add_edge(*edge, label='{:.2f}'.format(wt), penwidth=wt * 10)
         else:
-            self.plspm = PLSPMModel(self.Edges()).Fit(data)
-        return self
-
-    def AppendPLSWeights(self, data):
-        self.FitPLSPM(data)
-        for edge in self.Edges(): 
-            self.AddEdgeWeight(edge, self.plspm.GetEdgeCoef(edge))
-        return self
+            for edge in self.model_spec.Edges(include_type=False):
+                graph.add_edge(*edge)
+        
+        return graph
     
-    
-    def SubgraphFromNodes(self, node: list[str]): 
+    def SubgraphFromNodes(self, node: str | list[str]): 
+        
+        def _walk_down(from_: str, func):
+            for to_ in self.model_spec.OutEdges(from_):
+                func((from_, to_))
+                _walk_down(to_, func)
 
-        def _walk_down(graph: pgv.AGraph, node: str, func):
-            for e in graph.out_edges(node):
-                func(e)
-                _walk_down(graph, e[1], func)
+        def _walk_up(to_: str, func):
+            for from_ in self.model_spec.InEdges(to_):
+                func((from_, to_))
+                _walk_up(from_, func)
 
-        def _walk_up(graph: pgv.AGraph, node: str, func):
-            for e in graph.in_edges(node):
-                func(e)
-                _walk_up(graph, e[0], func)
-
-        def _copy_paste_edge(edge: pgv.agraph.Edge, graph: pgv.AGraph):
-            graph.add_edge(*edge, label=edge.attr['label'], penwidth=edge.attr['penwidth'])
+        def _add_edge(edge: tuple[str], graph: pgv.AGraph):
+            if self.plspm:
+                wt = self.plspm.GetPathCoef(edge)
+                graph.add_edge(*edge, label='{:.2f}'.format(wt), penwidth=wt * 10)
+            else:
+                graph.add_edge(*edge)
         
         if isinstance(node, str): 
             node = [node]
         elif not isinstance(node, list):
-            raise ValueError(".SubgraphFromNod: Wrong nodes definition")
+            raise ValueError(".SubgraphFromNode: Wrong nodes definition {}".format(node))
         
         sub_graph = pgv.AGraph(directed=True)
         
         for n in node: 
-            _walk_down(self.graph, n, lambda x: _copy_paste_edge(x, sub_graph))
-            _walk_up(self.graph, n,   lambda x: _copy_paste_edge(x, sub_graph))
+            _walk_down(n, lambda x: _add_edge(x, sub_graph))
+            _walk_up(  n, lambda x: _add_edge(x, sub_graph))
 
             key_node = sub_graph.get_node(n)
             key_node.attr['style'] = 'filled'
@@ -279,35 +274,40 @@ class GraphManipulations:
             key_node.attr['fontcolor'] = 'white'
             key_node.attr['fontsize'] = 24
 
-        return GraphManipulations(sub_graph)
+        return sub_graph
     
-    def RoutesLen(self, from_: str, to_: list[str] | str):
-        if isinstance(to_, str): 
-            to_ = [to_]
-        elif not isinstance(to_, list):
+    def PathLen(self, start_node: str, end_nodes: list[str] | str):
+        assert self.plspm, "Model not fit. Run .Fit... first"
+        if isinstance(end_nodes, str): 
+            end_nodes = [end_nodes]
+        elif not isinstance(end_nodes, list):
             raise ValueError(".RouteLen: Wrong to_ definition")
         
         total_route_len = 0
         
-        def _walk_down(graph: pgv.AGraph, node: str, carry):
+        def _walk_down(from_: str, carry):
             nonlocal total_route_len
-            for e in graph.out_edges(node):
-                carry_to_pass = carry * float(e.attr['penwidth']) / 10
-                if e[1] in to_: 
+            for to_ in self.model_spec.OutEdges(from_):
+                carry_to_pass = carry * self.plspm.GetPathCoef((from_, to_))
+                if to_ in end_nodes: 
                     total_route_len += carry_to_pass
                 else:
-                    _walk_down(graph, e[1], carry_to_pass)
+                    _walk_down(to_, carry_to_pass)
 
-        _walk_down(self.graph, from_, 1)
+        _walk_down(start_node, 1)
         return total_route_len
     
-    def RoutesLenFromAllNodes(self, to_: list[str] | str):
+    def PathLenFromAllNodes(self, end_nodes: list[str] | str):
+        if isinstance(end_nodes, str): 
+            end_nodes = [end_nodes]
+        elif not isinstance(end_nodes, list):
+            raise ValueError(".RouteLen: Wrong to_ definition")
+        
+        assert utils.AllElementsAreThere(end_nodes, self.model_spec.Nodes()), "Not all nodes are present in the model"
+        
         result = {}
-        for node in self.graph.nodes():
-            result[node] = self.RoutesLen(node, to_)
-        return result
+        for node in self.model_spec.Nodes():
+            result[node] = self.PathLen(node, end_nodes)
+        return pd.Series(result).sort_values(ascending=False)
 
-        
-        
-
-
+    
--- utils.py
+++ utils.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def CheckColumnsPresent(data:pd.DataFrame, cols: list) -> bool:
     return all([col in data.columns for col in cols])
+
+def AllElementsAreThere(elements: list, there: list) -> bool:
+    return all([e in there for e in elements])
+
+def ThereAreDoubles(arr):
+    return len(arr) != len(set(arr))
